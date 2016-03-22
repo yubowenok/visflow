3e22a46c551d5d87bd8123067248b480d5293384
Bowen Yu <yubowenok@gmail.com>
2016-03-22 19:01:04 -0400

    move visit log to db

--- index.php
+++ index.php
@@ -1,47 +1,7 @@
 <?php
 
-include 'server/response.php';
-
-if($_SERVER['SERVER_PORT'] != 443)
-{
-  status(301);
-  header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-  exit();
-}
+include 'server/session.php';
 
 echo file_get_contents('index.html');
-
-function get_client_ip()
-{
-  $ip_address = '';
-  if (getenv('HTTP_CLIENT_IP'))
-    $ip_address = getenv('HTTP_CLIENT_IP');
-  else if(getenv('HTTP_X_FORWARDED_FOR'))
-    $ip_address = getenv('HTTP_X_FORWARDED_FOR');
-  else if(getenv('HTTP_X_FORWARDED'))
-    $ip_address = getenv('HTTP_X_FORWARDED');
-  else if(getenv('HTTP_FORWARDED_FOR'))
-    $ip_address = getenv('HTTP_FORWARDED_FOR');
-  else if(getenv('HTTP_FORWARDED'))
-    $ip_address = getenv('HTTP_FORWARDED');
-  else if(getenv('REMOTE_ADDR'))
-    $ip_address = getenv('REMOTE_ADDR');
-  else
-    $ip_address = 'unknown';
-  return $ip_address;
-}
-
-date_default_timezone_set('America/New_York');
-$fp = fopen('visit.log', 'a');
-$ip = get_client_ip();
-$date = date('m/d/Y h:i:s A', time());
-$user = getenv('REMOTE_USER');
-fwrite($fp, "[$user $ip $date ts=".time().']');
-if(isset($_GET['diagram']))
-{
-  $x = $_GET['diagram'];
-  fwrite($fp, " diagram=$x");
-}
-fwrite($fp, "\n");
 
 ?>
--- server/session.php
+++ server/session.php
@@ -9,10 +9,31 @@ function checkLogin()
     abort('login required');
 }
 
+function clientIP()
+{
+  $ip_address = '';
+  if (getenv('HTTP_CLIENT_IP'))
+    $ip_address = getenv('HTTP_CLIENT_IP');
+  else if(getenv('HTTP_X_FORWARDED_FOR'))
+    $ip_address = getenv('HTTP_X_FORWARDED_FOR');
+  else if(getenv('HTTP_X_FORWARDED'))
+    $ip_address = getenv('HTTP_X_FORWARDED');
+  else if(getenv('HTTP_FORWARDED_FOR'))
+    $ip_address = getenv('HTTP_FORWARDED_FOR');
+  else if(getenv('HTTP_FORWARDED'))
+    $ip_address = getenv('HTTP_FORWARDED');
+  else if(getenv('REMOTE_ADDR'))
+    $ip_address = getenv('REMOTE_ADDR');
+  else
+    $ip_address = 'unknown';
+  return $ip_address;
+}
+
 if($_SERVER['SERVER_PORT'] != 443)
 {
-  // must query over https
-  status(403);
+  // redirect to https
+  status(301);
+  header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
   exit();
 }
 
@@ -26,6 +47,7 @@ session_start([
 $session_id = session_id();
 $username = '';
 $user_id = -1;
+$user_ip = clientIP();
 $auth_id = -1;
 
 $row = getOneDB("SELECT id AS auth_id, user_id, UNIX_TIMESTAMP(end_time) as end_time FROM auth WHERE session_id='%s'",
@@ -48,5 +70,8 @@ else
   queryDB("UPDATE auth SET end_time=FROM_UNIXTIME(%d) WHERE id=%d",
                  array($new_end_time, $auth_id));
 }
+
+queryDB("INSERT INTO visit (user_id, ip, session_id, url) VALUES (%d, '%s', '%s', '%s')",
+       array($user_id, $user_ip, $session_id, $_SERVER['REQUEST_URI']));
 
 ?>
