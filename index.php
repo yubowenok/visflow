916e9ec220f5f1bcb1efc2df8fd14c2c1eb09b64
Bowen Yu <yubowenok@gmail.com>
2016-03-31 12:30:40 -0400

    show unavailable msg to mobile clients

--- index.php
+++ index.php
@@ -2,6 +2,26 @@
 
 include 'server/session.php';
 
-echo file_get_contents('index.html');
+function isMobile(){
+  $keywords = array(
+    '/iphone/i' => 'iPhone',
+    '/ipod/i' => 'iPod',
+    '/ipad/i' => 'iPad',
+    '/android/i' => 'Android',
+    '/blackberry/i' => 'BlackBerry',
+    '/webos/i' => 'Mobile'
+  );
+  foreach($keywords as $key => $val)
+  {
+    if (preg_match($key, $_SERVER['HTTP_USER_AGENT']))
+      return true;
+  }
+  return false;
+}
+
+if (isMobile())
+  echo '<h1>Sorry, but we currently do not support mobile devices.</h1>';
+else
+  echo file_get_contents('index.html');
 
 ?>
