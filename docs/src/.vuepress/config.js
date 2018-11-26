0d62945cfc042b2bda45ce5bdcf70fcd3a5fb3f8
Bowen Yu <yubowenok@gmail.com>
2018-11-25 23:16:27 -0500

    fix router-link, use local font-awesome

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -4,7 +4,6 @@ module.exports = {
   description: 'Dataflow Framework for Visual Data Exploration',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['link', { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.2.0/css/all.css' }]
   ],
   themeConfig: {
     logo: '/logo.png',
